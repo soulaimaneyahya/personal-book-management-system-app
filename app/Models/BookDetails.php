fbae1fe1f24b6d55fae1c24cdee5f7906bb8b9d2
soulaimany <soulaiman.y@codpartner.com>
2024-12-24 17:49:22 +0100

    PBMS-1: feat-models

---
+++ app/Models/BookDetails.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BookDetails extends Model
+{
+    /** @use HasFactory<\Database\Factories\BookDetailsFactory> */
+    use HasFactory;
+}
