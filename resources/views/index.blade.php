5c1500e6057b2348ec622844dde469fc2bf9e792
soulaimany <soulaiman.y@codpartner.com>
2024-12-25 21:38:28 +0100

    PBMS-2: feat-auth

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,18 @@
-{{ config('app.name') }}
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ config('app.name') }}</div>
+
+                    <div class="card-body">
+                        <p>Magistra multitudinem amet aliquam quisque scipio gratiam inopem intellegi. Liberatione laborum conducunt sensum nostros mel pacuvii periculum unum. Delectat egregios partis umbram democrito laboriosam caelo. Obligantur discedere omittam detraxit uberius ferant terentii tempore. Assueverit animal poetarum didicisset causae nusquam commune libidinum amet. Efficeretur consoletur gloriosis varias possint labefactetur posteri paulo doctus.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+\Illuminate\Support\Facades\Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::get('/', function () {
     return view('index');
 });
